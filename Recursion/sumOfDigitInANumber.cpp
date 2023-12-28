b227683e8eeee51ed5768833f418dfd7ab591464
mohinirathore29 <mohinirathore404@gmail.com>
2023-12-28 20:49:24 +0530

    another way to find the sum of all digit in a number

--- Recursion/sumOfDigitInANumber.cpp
+++ Recursion/sumOfDigitInANumber.cpp
@@ -1,21 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+//method 1
+// void sumOfDigit(int n,int &sum){
+//     if(n<=0){
+//         return;
+//     }
+//     int digit = n%10;
+//     //cout<<"last digit "<<digit<<endl;
+//     sum = sum + digit;
+//     //cout<<"sum will be "<<sum<<endl;
+//     return sumOfDigit(n/10,sum);
+// }
 
-void sumOfDigit(int n,int &sum){
+//method 2
+int sumOfDigit(int n){
     if(n<=0){
-        return;
+        return n;
     }
-    int digit = n%10;
-    //cout<<"last digit "<<digit<<endl;
-    sum = sum + digit;
-    //cout<<"sum will be "<<sum<<endl;
-    return sumOfDigit(n/10,sum);
+    int lastDigit = n%10;
+    int remainingNum = n/10;
+    int finalSum = lastDigit + sumOfDigit(remainingNum);
+    return finalSum;
 }
 
 int main(){
     int num = 1234;
     int sum = 0;
-    sumOfDigit(num,sum);
-    cout<<"sum of all digit in "<<num<<" is :"<<sum<<endl;
+    //sumOfDigit(num,sum);
+    int finalSum = sumOfDigit(num);
+    //cout<<"sum of all digit in the number "<<num<<" is :"<<sum<<endl;
+    cout<<"sum of all digit in the number "<<num<<" is :"<<finalSum<<endl;
+
     return 0;
 }
